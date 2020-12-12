1b82cbe4ed9bca3bc83a6cda101cb299a7fa02e1
S-ecki <75510543+S-ecki@users.noreply.github.com>
2020-12-12 12:24:07 +0100

    Day 12

--- src/Day12.java
+++ src/Day12.java
@@ -12,13 +12,19 @@ public class Day12 extends Day{
 	}
 	
 	private class Ship{			//keeps track of position of ship and moves it
+		
+		private class Waypoint{		//keeps track of the Waypoint	-	always relative to ship
+			private int east = 10;	
+			private int north = 1;		//given starting position for east/north
+		}
+		
 		private int east = 0;
 		private int north = 0;
 		private char[] directions = new char[] {'E', 'S', 'W', 'N'};
 		private int currentDirection = 0;	//index of directions array
 		private Waypoint wayp = new Waypoint();		//to track waypoint of ship for solution 2
 		
-		private void doInstruction(Instruction instruction, Boolean s1) {	//s1 stand for Solution 1 - reusing code for solution 2
+		private void doInstruction(Instruction instruction, Boolean s1) {	//s1 stand for Solution 1 - reusing code for solution 2 (else)
 			switch(instruction.direction) {
 			case 'N': if(s1) north += instruction.value; else wayp.north += instruction.value; break;	//for solution 1, directly move ship
 			case 'E': if(s1) east += instruction.value; else wayp.east += instruction.value; break;		//for solutin 2, move waypoint
@@ -50,17 +56,13 @@ public class Day12 extends Day{
 		private void moveForward(int value) {
 			doInstruction(new Instruction(directions[currentDirection%4], value), true);	//this goes back to switch with correct direction to move to
 		}
-		private void moveToWaypointMultipleTimes(int value) {
+		private void moveToWaypointMultipleTimes(int value) {	//moves the ship on F-instruction for solution 2
 			for(int i = 0; i < value; ++i) {
 				this.east += wayp.east;
 				this.north += wayp.north;
 			}
 		}
-		
-		private class Waypoint{		//keeps track of the Waypoint	-	always relative to ship
-			private int east = 10;	
-			private int north = 1;		//given starting position for east/north
-		}
+	
 	}
 	
 //start of Day 12 class
@@ -79,9 +81,9 @@ public class Day12 extends Day{
 	
 	
 	public int getSolution(Boolean s1) {
-		Ship ship = new Ship();		//tracks state of ship and moves it
+		Ship ship = new Ship();
 			
-		for(Instruction i: instructions) ship.doInstruction(i, s1);		//do all instructions
+		for(Instruction i: instructions) ship.doInstruction(i, s1);
 		
 		return Math.abs(ship.east) + Math.abs(ship.north);		//return Manhattan Distance
 	}
